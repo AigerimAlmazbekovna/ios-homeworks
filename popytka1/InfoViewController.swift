ddb35d88708d6b88a8c9ca830dd04d739e0a562f
Aigerim Almazbekova <aigerimalmazbekovna1416@gmail.com>
2024-03-05 22:56:01 +0600

    Change navigationViewController

--- popytka1/InfoViewController.swift
+++ popytka1/InfoViewController.swift
@@ -7,57 +7,50 @@
 
 import UIKit
 
-class InfoViewController: UIViewController {
-    
-   
-    
-    
-    private lazy var actionButton: UIButton = {
-           let button = UIButton()
-           button.translatesAutoresizingMaskIntoConstraints = false
-           button.setTitle("Перейти", for: .normal)
-           button.setTitleColor(.systemBlue, for: .normal)
-           
-           return button
-       }()
-    
-   
-       override func viewDidLoad() {
-           super.viewDidLoad()
-           
-           view.addSubview(actionButton)
-          
-         
-           let safeAreaLayoutGuide = view.safeAreaLayoutGuide
-           NSLayoutConstraint.activate([
-               actionButton.leadingAnchor.constraint(
-                   equalTo: safeAreaLayoutGuide.leadingAnchor,
-                   constant: 20.0
-               ),
-               actionButton.trailingAnchor.constraint(
-                   equalTo: safeAreaLayoutGuide.trailingAnchor,
-                   constant: -20.0
-               ),
-               actionButton.centerYAnchor.constraint(equalTo: safeAreaLayoutGuide.centerYAnchor),
-               actionButton.heightAnchor.constraint(equalToConstant: 44.0)
-           ])
-           
-           actionButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
-       }
-    @objc func buttonPressed(_ sender: UIButton) {
-        let alertController = UIAlertController(title: "Заголовок", message:"описание задачи", preferredStyle: .alert)
-        let action1 = UIAlertAction(title: "Default", style: .default) { (action: UIAlertAction) in
-            print("Default")
-        }
+final class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGray6
         
-        let action2 = UIAlertAction(title: "Cancel", style: .cancel) { (action: UIAlertAction) in
-            print("Cancel")
-        }
-        alertController.addAction(action1)
-        alertController.addAction(action2)
-        
-        
-        present(alertController, animated: true, completion: nil)
+        createAlertButton()
     }
-   
+    
+    private func createAlertButton() {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Alert", for: .normal)
+        button.backgroundColor = .systemPink
+        button.setTitleColor(.white, for: .normal)
+    
+        button.addTarget(self, action: #selector(tapAlertButton), for: .touchUpInside)
+                
+        view.addSubview(button)
+        
+        NSLayoutConstraint.activate([
+            button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+            button.heightAnchor.constraint(equalToConstant: 50),
+            button.widthAnchor.constraint(equalToConstant: 100)
+        ])
+    }
+    
+    @objc func tapAlertButton() {
+        let alert = UIAlertController(title: "Attention",
+                                      message: "How are you feeling?",
+                                      preferredStyle: .alert)
+        // add two buttons
+        let fine = UIAlertAction(title: "Fine", style: .default) { _ in
+            print("Fine")
+        }
+        alert.addAction(fine)
+        
+        let so = UIAlertAction(title: "So-so", style: .destructive) { _ in
+            print("So-so")
+        }
+        alert.addAction(so)
+
+        self.present(alert, animated: true, completion: nil)
+    }
 }
---
+++ popytka1/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  popytka1
+//
+//  Created by Айгерим on 05.03.2024.
+//
+
+import Foundation
+
+struct Post {
+    let author: String
+    let description: String
+    let image: String
+    let likes: Int
+    let views: Int
+}
